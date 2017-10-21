6da1c63968566158cba640f9f076719f1230d6d0
Rahul Keshervani <keshervanirav16.comp@coep.ac.in>
2017-10-21 12:00:15 +0000

    ispell.h

---
+++ ispell.h
@@ -0,0 +1,10 @@
+/*all suggestions in an array*/
+	char words[20][20];
+
+int Master(Dict *d, char *str);
+char *removeletter(char *str);
+int mystrcmp(char *s1, char * s2, int miss);
+void printwords();
+/*file loading*/
+/*main function in ispell.c*/
+/**/
