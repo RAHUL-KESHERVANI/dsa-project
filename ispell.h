99ca6897408e024a0c6467eeb8766046ef840074
Rahul Keshervani <keshervanirav16.comp@coep.ac.in>
2017-10-31 23:00:48 +0000

    Replace ispell.h

--- ispell.h
+++ ispell.h
@@ -1,10 +1,18 @@
-/*all suggestions in an array*/
-	char words[20][20];
 
-int Master(Dict *d, char *str);
-char *removeletter(char *str);
+#define CORRECT 200
+#define INCORRECT -1
+#define SUGG 0
+#define delim "%!@#$^'&*()_+{}[]=;:\".,\0. ><?/-0123456789"
+void Master(Dict *d, char *str);
+
 int mystrcmp(char *s1, char * s2, int miss);
-void printwords();
-/*file loading*/
-/*main function in ispell.c*/
-/**/
+void printwords(Dict *d);
+void fprintwords(Dict *d, int r);
+int checkRejected(char *s, char **r, int i);
+void compareEqualString(Dict *d, char *s, char *str);
+char* toLower(const  char *s);
+int checkLower(char *p) ;
+int checkUpper(char * p);
+char* toUpper(char *s, char *str);
+int FMaster(Dict *d, char *str);
+
