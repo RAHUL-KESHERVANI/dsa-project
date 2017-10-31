61ba23307878cfc4d90b13598a7167c8482b3900
Rahul Keshervani <keshervanirav16.comp@coep.ac.in>
2017-10-31 23:23:30 +0000

    Replace dict.h

--- dict.h
+++ dict.h
@@ -9,12 +9,14 @@ typedef struct ele
 typedef struct{
 	ele *table;
 	int size;
+	char ** words;
+	int i;
 }Dict;
-
+void reallocWords(Dict *d);
 void init(Dict *d, int size);
 char * Search(Dict *d, char*str) ;
 void PrintD(Dict *d);
 void Load(Dict *d, char *str);
 long int hash(char *str);
-
+void DestroyTbl(Dict *d);
 int hashcompare(long int h1, long int h2);
