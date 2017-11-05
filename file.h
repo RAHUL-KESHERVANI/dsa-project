d915b8f2c9f257760691a2c519380b1ea3881612
Rahul Keshervani <keshervanirav16.comp@coep.ac.in>
2017-11-05 12:09:31 +0000

    Replace file.h

--- file.h
+++ file.h
@@ -1,14 +1,15 @@
 typedef struct file {
 	char filename[50];
 	char *fileline[2];
+	char *line;
+	int repl;
 	char *str;
 	int i;
-	int flag;
-	int rows;
-	int columns;
+	int start;
+	int end;
+	int br;
 }file;
 
 void initf(file *f, int rows);
 void printfile(file *f, Dict *d);
-void Options(file *f, char **w);
 void Option_R(file *f);
