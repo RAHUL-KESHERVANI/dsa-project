d2adbb434e6a6084c2d70bffb00b7b10ab970934
Rahul Keshervani <keshervanirav16.comp@coep.ac.in>
2017-11-05 12:08:41 +0000

    Replace ispell.c

--- ispell.c
+++ ispell.c
@@ -1,3 +1,31 @@
+
+/**************************************************************************
+ *   ispell.c  --  This file is a part of DSA miniproject.                *
+ *                                                                        *
+ *   Copyright 2017   (C)   Rahul Keshervani                            *
+ *                                                                        *
+ *   ispell is free software: you can redistribute it and/or modify     *
+ *   it under the terms of the GNU General Public License as published    *
+ *   by the Free Software Foundation, either version 3 of the License,    *
+ *   or (at your option) any later version.                               *
+ *                                                                        *
+ *   ispell is distributed in the hope that it will be useful,          *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty          *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.              *
+ *   See the GNU General Public License for more details.                 *
+ *                                                                        *
+ *   You should have received a copy of the GNU General Public License    *
+ *   along with this program.  If not, see http://www.gnu.org/licenses/.  *
+ *                                                                        *
+ **************************************************************************/
+         /*OPTIONS FOR ISPELL IN FILE MODE*/ 
+         /*6 options are there available R for replace*/
+         /*number for replaing the suggested words*/
+         /*A for accept the word for the rest of session*/
+         /*space for ignore*/
+         /*I for adding the word to the dictionary*/
+         /*U for adding the word to the dictionary in lowercase*/
+         /*Q for quiting*/
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -26,6 +54,7 @@ int getWindowSize(int *rows, int *cols) {
     return 0;
   }
 }
+
 void enableRawMode() {
   tcgetattr(STDIN_FILENO, &orig_termios);
   atexit(disableRawMode);
@@ -38,73 +67,103 @@ void clrscr() {
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 void initf(file *f, int columns){
-  f->fileline[0] = (char *)malloc(columns);
-  f->fileline[1] = (char *)malloc(columns);
-  strcpy(f->fileline[0], "\0");
-  // strcpy(f->fileline[1], "\0");
+  f->fileline[0] = (char *)malloc(columns+2);
+  f->fileline[1] = (char *)malloc(columns+2);
+  f->line = (char *)malloc(columns+2);
+  strcpy(f->fileline[0], "\n");
+  strcpy(f->fileline[1], "\0");
   f->i = 0;
-  f->flag = 0;
+  f->repl = 0;
+  f->start = 0;
+  f->end = 0;
+}
+void updateLine(file *f, int w) {
+  int l = strlen(f->line);
+  int ls = strlen(f->str);
+  int extra, i;
+  strcpy(f->fileline[1], "\0");
+  if(l < w){
+    f->br = f->i - ls;
+    f->end = l;
+  }
+  else {
+    if(f->i < w){
+      f->br = f->i - ls;
+      f->end = w;
+    }
+    else {
+      
+      if(ls == w - 34)
+        extra = 34;
+      else if(f->i == l)
+        extra = w - ls -34;
+      else
+        extra = w-ls;
+      
+      f->start = f->i - ls - extra;
+      f->end = f->i;
+      f->br = f->i - ls - f->start;
+    }
+  }
+  for (i = f->start; i < f->end; ++i)
+  {
+    f->fileline[1][i - f->start] = f->line[i];
+  }
+  f->fileline[1][i-f->start] = '\0';
 }
 int getfline(file *f, FILE *fpr, int columns) {
-  int  i= 0, extra, j;
-  char c;
+  int j;
+  long int n;
+  j = getline(&(f->line), &n, fpr);
   f->i = 0;
-  int l = strlen(f->fileline[0]);
-   if(l == columns){
-    if((c = fgetc(fpr)) != EOF && isalpha(c) && isalpha(f->fileline[0][l - 1])){
-      // printf("fdasdsf\n");
-      f->flag = 1;
-    }
-    else{
-    f->fileline[1][i] = c;
-    i++;
-  }
-  }
-  if(f->flag)
-  {
-    extra = 25 - strlen(f->str);
-    j = extra;
-    while(j >= 0){
-      f->fileline[1][i++] = f->fileline[0][l - j];
-      j--;
-    }
-    strcat(f->fileline[1], f->str);
-    i += strlen(f->str);
-    f->fileline[1][i++] = c;
-    f->flag = 0;
-  }
- 
-  while(i < columns && ((c = fgetc(fpr)) != EOF) && c != '\n') {  
-    f->fileline[1][i] = c;  
-    i++;
-  }
-  f->fileline[1][i] = '\0';
-  // printf("%s\n", f->fileline[1]);
-  if(!i)
-    return EOF;
-
-  return 1;
+  f->start = 0;
+  f->end = 0;
+  f->br = 0;
+  return j;
 }
-
+void DestroyFile(file *f) {
+  free(f->fileline[0]);
+  free(f->fileline[1]);
+  free(f->line);
+}
 void OptionsR(file *f) {
-  char str[160], *p, c;
-  int i = 0;
-  // if(f->i != strlen(f->fileline[1]))
-  p = strdup(f->fileline[1]+f->i);
- 
-printf("Replace With:");
+  char str[160], *p, c, *q;
+  int i = 0, j;
+  p = (char*)malloc(173);
+  q = strdup(f->line);
+  printf("Replace With:");
+  
   disableRawMode();
   while((c = getchar()) != '\n')
     str[i++] = c;
-enableRawMode();
-str[i] = '\0';
-  f->fileline[1][f->i - strlen(f->str)] = '\0';
-  strcat(f->fileline[1], str);
-  strcat(f->fileline[1], p);
- 
+  enableRawMode();
+  
+  str[i] = '\0';
+  
+  for (j = 0; j < f->i - strlen(f->str); ++j)
+    p[j] = f->line[j];
+  p[j] = '\0';
+  
+  strcpy(f->line, p);
+  strcat(f->line, str);
+  strcat(f->line, q+f->i);
+  str[i] = '\0';
+  f->end = f->end - strlen(str) + strlen(f->str);
   f->i = f->i - strlen(f->str);
   strcpy(f->str, str);
+
+  for (i = f->start; i < f->end; ++i)
+    f->fileline[1][i - f->start] = f->line[i];
+  
+  f->fileline[1][i-f->start] = '\0';
+  f->repl = replaced;
+  if(f->i < 0)
+    f->i = 0;
+  
+  free(p);
+  free(q);
 }
+
 #define KRED  "\x1B[31m"
 #define KNRM  "\x1B[0m"
 
@@ -113,28 +172,39 @@ void printfile(file *f, Dict *d) {
   int r, c;
   char *p;
   p = strdup(f->fileline[1]);
-  p[f->i -strlen(f->str)] = '\0';
   getWindowSize(&r,&c);
-  printf("\t%s\t\t%s\n", f->str, f->filename);
-  printf("%s\n", f->fileline[0]);
+  if(strtok(f->fileline[1], "\n") != NULL)
+    f->fileline[1][strlen(f->fileline[1])] = '\0';
+
+  printf("\t");
+  printf("%s", f->str);
+  if(strlen(f->str) < c/4)
+  printf("\t\t");
+  printf(" file :%s\n\n",f->filename);
+  printf("%s", f->fileline[0]);
+  p[f->br] = '\0';
   printf("%s",p);
-  printf("%s%s", KRED, f->str); 
-  printf("%s%s", KNRM,f->fileline[1]+f->i);
-  printf("\n\n");
-  // if(d->i >0)
+  printf("%s%s", KRED, f->str);
+  printf("%s%s", KNRM, f->fileline[1]+f->br + strlen(f->str));
+  printf("\n");
   fprintwords(d, r);
   printf("[SP] <number> R)epl A)ccept I)nsert U)ncap Q)uit\n");
   free(p);
 }
 char *mystrtok(file *f, int w) {
-  int i, j, flag = 1, len;
-  char *p = malloc(w);
-  int pointer;
-  for (i = f->i;i <= strlen(f->fileline[1]); ++i)
+  int i, j, flag = 1  , len;
+  char *p = malloc(w-34);
+  int pointer = -1;
+  if(f->repl == replaced){
+    f->i = f->i + strlen(f->str);
+    f->repl = not_repl;
+    return f->str;
+  }
+    for (i = f->i;i <= strlen(f->line); ++i)
   {
     for (j = 0; j < strlen(delim); ++j)
     {
-      if(f->fileline[1][i] == delim[j] || f->fileline[1][i] == '\0' || f->fileline[1][i] == ' ')
+      if(f->line[i] == delim[j] || f->line[i] == '\0' || f->line[i] == ' ' || f->line[i] == '\n')
         break;
     }
     if(j == strlen(delim) && flag) {
@@ -147,43 +217,81 @@ char *mystrtok(file *f, int w) {
     }
   }
   if(!flag) {
+ if(pointer == -1)
+      return NULL;
     len = f->i - pointer;
-    if(len > w - 20){
-      f->i = f->i - len + w - 20;
-      len = len - w + 20;
+    if(len > w - 34){
+      f->i = f->i - len + w - 34;
+      len = w-34;
     }
-    for (int i = 0; i < len; ++i)
-    {
-      p[i] = f->fileline[1][i+pointer];
-    }
+    for (int i = 0; i < len; i++)
+      p[i] = f->line[i+pointer];
+    p[len] = '\0';
     f->str = p;
+    updateLine(f, w);
     return p;
   }
   return NULL;
 
 }
-void cpyfile(char *file) {
+#define Quit 1
+#define Ignore 2
+int sugg(file *f, Dict *d, int i) {
   char c;
-  FILE *fp1 = fopen(file, "w");
-  FILE *fp2 =fopen("myfile", "r");
-  while((c = getc(fp2)) != EOF) {
-    putc(c, fp1);
+  char *p = strdup(f->line+ f->i), *str;
+  int j;
+  if(d->i > 9) {
+    if(i > 1) {
+      while(read(STDIN_FILENO, &c, 1) == 1){
+        if(c == 'Q')
+          return Quit;
+        if(c == ' ')
+          return Ignore;
+        i = c - '0';
+        if(i <= 1)
+          break;
+      }
+    }
+    while(read(STDIN_FILENO, &c, 1) == 1){
+      if(c == 'Q')
+          return Quit;
+        if(c == ' ')
+          return Ignore;
+          if(!isdigit(c))
+      continue;
+      j = i;
+      i= i *10 + c - '0';
+      if(i > d->i)
+        i = j;
+      else
+        break;
+    }
   }
-}
-void sugg(file *f, Dict *d, int i) {
-  char c;
-  char *p = strdup(f->fileline[1]+ f->i), *str;
-   if(d->i > 9){
-    while(read(STDIN_FILENO, &c, 1) == 1 && !isdigit(c));
-    i= i *10 + c - '0';
+  else if(i > d->i){
+    while(read(STDIN_FILENO, &c, 1) == 1){
+         if(c == 'Q')
+          return Quit;
+        if(c == ' ')
+          return Ignore;
+      if(!isdigit(c))
+      continue;
+
+      if(c - '0' <= d->i){
+        i = c - '0';
+        break;
+      }
+    }
   }
-  f->fileline[1][f->i - strlen(f->str)] = '\0';
-  
-  strcat(f->fileline[1], d->words[i]);
-  strcat(f->fileline[1], p);
- 
-  f->i = f->i - strlen(f->str);
-  strcpy(f->str, d->words[i]);
+  f->line[f->i - strlen(f->str)] = '\0';
+  strcat(f->line, d->words[i]);
+  strcat(f->line, p);
+  f->end = f->end + strlen(d->words[i]) - strlen(f->str);
+  f->i = f->i + strlen(d->words[i]) - strlen(f->str);
+  for (i = f->start; i < f->end && i < strlen(f->line); ++i)
+    f->fileline[1][i - f->start] = f->line[i];
+  f->fileline[1][i-f->start] = '\0';
+  return 0;
+
 }
 int getFileLength(char *f) {
   FILE *fp = fopen(f, "r");
@@ -196,8 +304,8 @@ int getFileLength(char *f) {
 }
 int main(int argc, char*argv[]) {
   Dict d;
-  char buf[512], fbuf[512], **rejected;
-  int i, len, j, rej, flag;
+  char buf[512], fbuf[512];
+  int i, len, j, rej, flag, k;
   FILE *fp, *fpr, *fpw;
   char c, *str;
   fp = fopen("british-english", "a+");
@@ -224,6 +332,8 @@ int main(int argc, char*argv[]) {
     	return errno;
     }
     fpw = fopen("myfile", "w");
+    if(fpw == NULL)
+      printf("error\n");
     clrscr();
     enableRawMode();
     char c, *p;
@@ -233,20 +343,32 @@ int main(int argc, char*argv[]) {
     initf(&f, w);
      strcpy(f.filename, argv[1]);
     while((getfline(&f, fpr, w)) != EOF) {
-      printf("%s\n", f.fileline[1]);
       str = mystrtok(&f, w);
       flag = 0;
+
       while(str != NULL) {
         i = FMaster(&d, str);
         if(i != CORRECT){
           printfile(&f, &d);
-         while (read(STDIN_FILENO, &c, 1) == 1 && c != 'R' && c != ' ' && c != 'A' && c != 'Q' && c!='I' && c!='U'&& !isdigit(c));
+         while (read(STDIN_FILENO, &c, 1) == 1 && c != 'R' && c != ' ' && c != 'A' && c != 'Q' && c!='I' && c!='U'&& (d.i == 0 || !isdigit(c)));
+         /*6 options are there available R for replace*/
+         /*number for replaing the suggested words*/
+         /*A for accept the word for the rest of session*/
+         /*space for ignore*/
+         /*I for adding the word to the dictionary*/
+         /*U for adding the word to the dictionary in lowercase*/
+         /*Q for quiting*/
          switch(c){
          case 'R':
           OptionsR(&f);
           break;
           case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':
-          sugg(&f, &d, c - '0');
+          k = sugg(&f, &d, c - '0');
+          if(k == Quit){
+            clrscr();
+            return 0;
+          }
+          break;
           case ' ':
           clrscr();
           break;
@@ -266,21 +388,32 @@ int main(int argc, char*argv[]) {
        }
           
        }
-
         (str = mystrtok(&f, w));
         reallocWords(&d);
       }
-        fwrite(f.fileline[1], 1 , strlen(f.fileline[1]), fpw);
-        fwrite("\n", 1, 1, fpw);
-        strcpy(f.fileline[0], f.fileline[1]);
+      clrscr();
+      fwrite(f.line, 1 , strlen(f.line), fpw);
+      l = strlen(f.line);
+        if(l > w){
+          f.line[w] = '\n';
+          l = w + 1;
+        }
+        f.line[l] = '\0';
+        strcpy(f.fileline[0], f.line);
         strcpy(f.fileline[1], "\0");
     }
     fclose(fpr);
-     fclose(fpw);
-    cpyfile(argv[1]);
+    fclose(fpw);
+    /*copying the edited tempfile into the original file*/
+    fpw = fopen(argv[1], "w");
+    fpr =fopen("myfile", "r");
+    while((c = getc(fpr)) != EOF) {
+      putc(c, fpw);
+    }
+    DestroyFile(&f);
   }
   DestroyTbl(&d);
+  
   fclose(fp);
   
- 
 }
